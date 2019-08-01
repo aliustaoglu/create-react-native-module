790b0683b947a1bfd089147322908d7c78700ec0
Christopher J. Brody <chris@brody.consulting>
2019-07-31 23:08:00 -0400

    XXX TBD no more mockable jsonfile object - RED (npm test is KNOWN TO FAIL in this commit)

--- tests/helpers/io-mocks.js
+++ tests/helpers/io-mocks.js
@@ -16,23 +16,20 @@ ${content}
       mysnap.push(`* ensureDir dir: ${dir}\n`);
       return Promise.resolve();
     },
+    readFileSync: (jsonPath) => {
+      mysnap.push(
+        `* fs readFileSync jsonPath: ${jsonPath}\n`);
+      return `{ "name": "bogus", "scripts": {} }`;
+    },
+    writeFileSync: (path, json, options) => {
+      mysnap.push(
+        `* fs writeFileSync path: ${path} json ${json} options ${JSON.stringify(options)}\n`);
+    },
   },
   execa: {
     commandSync: (command, options) => {
       mysnap.push(
         `* execa.commandSync command: ${command} options: ${JSON.stringify(options)}\n`);
-    },
-  },
-  jsonfile: {
-    readFileSync: (jsonPath) => {
-      mysnap.push(
-        `* jsonfile.readFileSync jsonPath: ${jsonPath}\n`);
-      return { name: 'bogus', scripts: {} };
-    },
-    writeFileSync: (path, json, options) => {
-      mysnap.push(
-        `* jsonfile.writeFileSync path: ${path} json ${JSON.stringify(json)} options ${JSON.stringify(options)}\n`);
-      mysnap.push(json);
     },
   },
 });
